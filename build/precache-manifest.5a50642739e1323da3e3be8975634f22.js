3b2dec8347a8ca42c70166ea6da3fb4679278227
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-06-19 03:28:13 -0300

    Added gh-pages as dev dependency due further deployment

---
+++ build/precache-manifest.5a50642739e1323da3e3be8975634f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485322e8b4336da7c592a9309710c1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8264a5a31b72cb7fa20b",
+    "url": "/static/css/main.e94ee128.chunk.css"
+  },
+  {
+    "revision": "450e7c9a8f5b70c438a8",
+    "url": "/static/js/2.02e9f5c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02e9f5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8264a5a31b72cb7fa20b",
+    "url": "/static/js/main.76dcb570.chunk.js"
+  },
+  {
+    "revision": "ed8656bb8ad7ae52878f",
+    "url": "/static/js/runtime-main.d6610f43.js"
+  }
+]);
